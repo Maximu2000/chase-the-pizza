f789e5e6fb563feb368ac37eb3e5e4319d3667ae
Maximu2000 <77606552+Maximu2000@users.noreply.github.com>
2021-01-18 15:20:50 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,44 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    Taco.setPosition(randint(10, 160), randint(10, 120))
+    info.startCountdown(10)
+})
+let Taco: Sprite = null
+scene.setBackgroundColor(2)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . f f f f f f f f . . . . . 
+    . . . f 9 9 9 9 9 9 f . . . . . 
+    . . . f 9 f 9 9 f 9 f . . . . . 
+    . . . f 9 f 9 9 f 9 f . . . . . 
+    . . . f 9 f 9 9 f 9 f . . . . . 
+    . . . f 9 9 9 9 9 9 f . . . . . 
+    . . . f 9 9 9 9 9 9 f . . . . . 
+    . . . f f f f f f f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+Taco = sprites.create(img`
+    . . . . . . . e e e e . . . . . 
+    . . . . . e e 4 5 5 5 e e . . . 
+    . . . . e 4 5 6 2 2 7 6 6 e . . 
+    . . . e 5 6 6 7 2 2 6 4 4 4 e . 
+    . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
+    . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
+    . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
+    e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
+    e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
+    e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
+    e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
+    e 5 e c 5 4 5 4 5 5 5 e e . . . 
+    e 5 e e 5 5 5 5 5 4 e . . . . . 
+    4 5 4 e 5 5 5 5 e e . . . . . . 
+    . 4 5 4 5 5 4 e . . . . . . . . 
+    . . 4 4 e e e . . . . . . . . . 
+    `, SpriteKind.Food)
